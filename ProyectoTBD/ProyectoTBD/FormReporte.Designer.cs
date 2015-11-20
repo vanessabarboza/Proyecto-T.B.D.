b853096a55c0d36ab9f725b83e60c79079395310
rigo9412 <rigo.ramos.9412@hotmail.com>
2015-11-20 08:17:48 -0600

    Controles con estilo metro

--- ProyectoTBD/ProyectoTBD/FormReporte.Designer.cs
+++ ProyectoTBD/ProyectoTBD/FormReporte.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 545);
+            this.Name = "FormReporte";
             this.Text = "FormReporte";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
